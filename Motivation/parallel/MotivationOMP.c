37158a668a441d6daf1f476f84548e9f21734682
Shinjae Yoo <sjyoo@bnl.gov>
2014-08-18 12:35:20 -0400

    Fixed num_threads(8)

--- Motivation/parallel/MotivationOMP.c
+++ Motivation/parallel/MotivationOMP.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char** argv) {
 
-#pragma omp parallel
+#pragma omp parallel num_threads(8) 
 	{
 		for (int i = INT_MIN; i < INT_MAX; i++) ;
 		printf("Done with pass %d\n",omp_get_thread_num());
--- setenv.bash
+++ setenv.bash
@@ -2,4 +2,4 @@ export CC=gcc
 export FC=gfortran
 export CFLAGS=-fopenmp
 export FFLAGS=-fopenmp
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=8
